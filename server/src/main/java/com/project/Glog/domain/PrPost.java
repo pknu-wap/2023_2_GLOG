ca9a0a45c781ac7fa5bfb3511994efe0c76d981f
JongKyeong Kim <oo7bangjk@naver.com>
2024-07-13 00:45:45 +0900

    연쇄 삭제 수정

--- server/src/main/java/com/project/Glog/domain/PrPost.java
+++ server/src/main/java/com/project/Glog/domain/PrPost.java
@@ -18,7 +18,7 @@ public class PrPost {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
     private Post post;
     @ManyToOne(fetch = FetchType.LAZY)
     private Category category;
